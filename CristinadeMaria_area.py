442bcf4cfa665837faab17d1f31c35a80263a1af
criissdm126 <128620048+criissdm126@users.noreply.github.com>
2023-11-10 09:27:04 +0100

    10/11/2023

--- CristinadeMaria_area.py
+++ CristinadeMaria_area.py
@@ -1,19 +1,32 @@
 # Enunciado: Hacer un programa que calcula el área de la figura descomponiéndola en hilos para optimizar.
 import threading
 
-def area_figura(hilo1, hilo2, hilo3, num_hilos):
-    area_hilo = (hilo1 + hilo2 + hilo3) * 0.01
-    area_total = area_hilo * num_hilos
-    return area_total
+# Triángulo grande
+base_triangulo_grande = 10
+altura_triangulo_grande = 12
+area_triangulo_grande = 0.5 * base_triangulo_grande * altura_triangulo_grande
 
-# Parámetros de la figura
-hilo1 = 26
-hilo2 = 8
-hilo3 = 6
-num_hilos = 100000 # Valor más pequeño para probar
 
-# Calcular el área total de la figura
-area_total = area_figura(hilo1, hilo2, hilo3, num_hilos)
+# Rectángulo grande
+base_rectangulo_grande = 8
+altura_rectangulo_grande = 12
+area_rectangulo_grande = base_rectangulo_grande * altura_rectangulo_grande
 
-# Imprimir el resultado
-print("El área total de la figura es:", area_total)
+
+# Rectángulo pequeño
+base_rectangulo_pequeño = 6
+altura_rectangulo_pequeño = 5
+area_rectangulo_pequeño = base_rectangulo_pequeño * altura_rectangulo_pequeño
+
+# Triángulo pequeño
+base_triangulo_pequeño = 2
+altura_triangulo_pequeño = 5
+area_triangulo_pequeño = 0.5 * base_triangulo_pequeño * altura_triangulo_pequeño
+
+# Base de la figura principal
+base_figura_principal = 26
+
+# Área de la figura principal, suma de las áreas de las figuras anteriores
+area_figura_principal = area_triangulo_grande + area_rectangulo_grande + area_rectangulo_pequeño + area_triangulo_pequeño
+
+print(f"El área de la figura principal es: {area_figura_principal}")
